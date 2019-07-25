933ac6369ba4a07811866491068dd63077e4a52a
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2019-07-25 16:40:32 -0400

    stylesheets for all px widths

--- admin/admin/admin.php
+++ admin/admin/admin.php
@@ -12,7 +12,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>BBS | Admin Center</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans+Condensed:300|Playfair+Display&display=swap" rel="stylesheet"/>
+    <!-- Width: 0px to 360px (Default CSS) -->
     <link rel="stylesheet" type="text/css" href="style/admin_360.css" />
+    <!-- Width: 361px to 375px -->
+    <link rel="stylesheet" media="screen and (min-width: 361px) and (max-width: 375px)" href="style/admin_375.css"/>
+    <!-- Width: 376px to 414px -->
+    <link rel="stylesheet" media="screen and (min-width: 376px) and (max-width: 414px)" href="style/admin_414.css"/>
+    <!-- Width: 415px to 768px -->
+    <link rel="stylesheet" media="screen and (min-width: 415px) and (max-width: 768px)" href="style/admin_768.css"/>
+    <!-- Width: 769px to 1366px -->
+    <link rel="stylesheet" media="screen and (min-width: 769px) and (max-width: 1366px)" href="style/admin_1366.css"/>
+    <!-- Width: 1367px to 1440px -->
+    <link rel="stylesheet" media="screen and (min-width: 1367px) and (max-width: 1440px)" href="style/admin_1440.css"/>
+    <!-- Width: 1441px and above -->
+    <link rel="stylesheet" media="screen and (min-width: 1441px)" href="style/admin_1920.css"/>
     <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
