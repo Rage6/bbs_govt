94eee2ecf6ce8dc3b7ef727fed2eade13a5b4d9b
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2019-07-26 15:30:29 -0400

    set up for deploy

--- pdo.php
+++ pdo.php
@@ -4,12 +4,10 @@
 
   if ($currentHost == "localhost:8888") {
     $pdo = new PDO('mysql:host=localhost;port=8888;dbname=BBS_government','Nick','Ike');
-    $hostStatus = "Using laptop database";
     $rootURL = $currentHost."/Buckeye_Boys_State/bbs_govt/";
   } else {
-    // remote dB connection goes here
-    $hostStatus = "Using remote server";
-    // actual URL goes here
+    $pdo = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;port=3306;dbname=heroku_9f89bb0196fa398','bb859affb4aa30','*passwrd_goes_here*');
+    $rootURL = $currentHost;
   };
 
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
