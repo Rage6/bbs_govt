612e4a6d115822b3c29ae96bf44b1488d879cf58
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-01-21 00:07:54 -0500

    carried gov js to SC for Minutes

--- state/supreme_court/main.js
+++ state/supreme_court/main.js
@@ -11,11 +11,26 @@ $(()=>{
     };
   });
 
-  // The function that
   // Closes the menu options when an option is selected
   $("div.optionBttn").click(()=>{
     if (window.innerWidth < 769) {
       $(".allOptions").css("display","none");
     };
   });
+
+  // Selects the 'minutes' to see
+  $("#minute1").css("color","#2020a0").css("background-color","#fec231");
+  $("#minuteCnt1").css("display","block");
+  $("[data-day]").click((event)=>{
+    $("#" + event.target.id).css("color","#2020a0").css("background-color","#fec231");
+    let minuteNum = parseInt($("#" + event.target.id).attr("data-day"));
+    $("#minuteCnt" + minuteNum).css("display","block");
+    for (let c = 1; c < 6; c++) {
+      if (c != minuteNum) {
+        $("#minute" + c).css("color","#fec231").css("background-color","#2020a0");
+        $("#minuteCnt" + c).css("display","none");
+      };
+    };
+  });
+
 })
