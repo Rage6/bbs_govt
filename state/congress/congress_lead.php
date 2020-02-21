9c6e11471c8c4494a8c51c308ae1a08e2517abd6
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-02-20 22:30:38 -0500

    before removing the side buttons

--- state/congress/congress_lead.php
+++ state/congress/congress_lead.php
@@ -6,9 +6,18 @@
     return true;
   };
 
+  // FOR SENATE
+
   // Gets Senate's introductory statement
   $senIntroStmt = $pdo->prepare("SELECT content FROM Post WHERE post_id=30");
   $senIntroStmt->execute();
   $senIntro = $senIntroStmt->fetch(PDO::FETCH_ASSOC);
+
+  // FOR HOUSE OF REPRESENTATIVES
+
+  // Gets House of Rep's introductory statement
+  $houseIntroStmt = $pdo->prepare("SELECT content FROM Post WHERE post_id=22");
+  $houseIntroStmt->execute();
+  $houseIntro = $houseIntroStmt->fetch(PDO::FETCH_ASSOC);
 
 ?>
