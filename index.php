df3381840d8a4c8b831a99abd977a67f405ef78d
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-04-08 15:26:23 -0400

    put background setup for max width

--- index.php
+++ index.php
@@ -44,180 +44,181 @@
     <script src="main.js"></script>
   </head>
   <body>
-    <div class="adminLink">
-      <a href="admin/login/login.php">
-        <img src="img/gear.png" />
-      </a>
-    </div>
-    <div id="hubTitle">
-      <div>Buckeye Boys State</div>
-      <div>DIRECTORY</div>
-    </div>
-    <div id="glossaryBttn" class="glossaryBar">
-      MENU
-    </div>
-    <div id="glossaryBox" class="glossaryBox">
-      <div id="numbersBttn">BASIC STATISTICS</div>
-      <div id="stateBttn">STATE</div>
-      <div id="countyBttn">COUNTY</div>
-      <div id="cityBttn">CITY</div>
-      <!-- <div id="electBttn">ELECTION PROCESS</div> -->
-      <div id="aboutBttn">ABOUT BBS</div>
-    </div>
-    <div class="introRow">
-      <div class="introBox">
-        <span class="introTitle">W</span>elcome to the Buckeye Boys State (BBS) government directory. Here you will find:
-        <ul>
-          <li>Basic statistics about BBS</li>
-          <li>Links to every branch of the state-level government</li>
-          <li>Populations & Flags awarded to every county and city</li>
-          <li>A short summary of the BBS program</li>
-          <!-- <li>Election process for each elected official</li> -->
-        </ul>
-        Use the 'MENU' button to find your desired information.
+    <div class="indexMain">
+      <div class="adminLink">
+        <a href="admin/login/login.php">
+          <img src="img/gear.png" />
+        </a>
       </div>
-      <div id="numbersTop" class="statsBox">
-        <div class="statsTitle"><span class="introTitle">B</span>y the numbers...</div>
-        <div>Population: <?php echo($totalPopulation) ?></div>
-        <div>Start Date: <?php echo($startDate) ?></div>
-        <div>End Date: <?php echo($endDate) ?></div>
-        <div>Location: Oxford, OH</div>
-        <div>BBS Counties: <?php echo($totalCounties) ?></div>
-        <div>BBS Cities: <?php echo($totalCities) ?></div>
+      <div id="hubTitle">
+        <div>Buckeye Boys State</div>
+        <div>DIRECTORY</div>
       </div>
-    </div>
-    <div id="hubContent">
-      <div class="govtLinkRow">
-        <div id="stateTop">
-          <div class="levelTitle">
-            STATE
-          </div>
-          <div class="sectionList">
-            <a href="state/governor/governor.php">
-              <div class="levelButton">
-                <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
-                  <div>
-                    Office of the Governor
-                  </div>
-                  <div>
-                    <div><img src='img/right_arrow.png'></div>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a href="state/congress/congress.php">
-              <div class="levelButton">
-                <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
-                  <div>
-                    General Assembly
-                  </div>
-                  <div>
-                    <div><img src='img/right_arrow.png'></div>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a href="state/supreme_court/supreme_court.php">
-              <div class="levelButton">
-                <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
-                  <div>
-                    Supreme Court
-                  </div>
-                  <div>
-                    <div><img src='img/right_arrow.png'></div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
+      <div id="glossaryBttn" class="glossaryBar">
+        MENU
+      </div>
+      <div id="glossaryBox" class="glossaryBox">
+        <div id="numbersBttn">BASIC STATISTICS</div>
+        <div id="stateBttn">STATE</div>
+        <div id="countyBttn">COUNTY</div>
+        <div id="cityBttn">CITY</div>
+        <!-- <div id="electBttn">ELECTION PROCESS</div> -->
+        <div id="aboutBttn">ABOUT BBS</div>
+      </div>
+      <div class="introRow">
+        <div class="introBox">
+          <span class="introTitle">W</span>elcome to the Buckeye Boys State (BBS) government directory. Here you will find:
+          <ul>
+            <li>Basic statistics about BBS</li>
+            <li>Links to every branch of the state-level government</li>
+            <li>Populations & Flags awarded to every county and city</li>
+            <li>A short summary of the BBS program</li>
+            <!-- <li>Election process for each elected official</li> -->
+          </ul>
         </div>
-        <div id="countyTop">
-          <div class="levelTitle">
-            COUNTY
-          </div>
-          <div class="sectionList">
-          <?php
-            for ($countyNum = 0; $countyNum < count($countyList); $countyNum++) {
-              $cntyPopStmt = $pdo->prepare("SELECT SUM(population) FROM Section WHERE is_county=:si");
-              $cntyPopStmt->execute(array(
-                ':si'=>htmlentities($countyList[$countyNum]['section_id'])
-              ));
-              $cntyPop = $cntyPopStmt->fetch(PDO::FETCH_ASSOC)['SUM(population)'];
-              echo("
-                <!-- <a href='county/county.php?section_id=".$countyList[$countyNum]['section_id']."'> -->
-                  <div class='levelButton'>
-                    <div class='sectionName'>
-                      <div>".$countyList[$countyNum]['section_name']." County</div>
-                      <!-- <div><img src='img/right_arrow.png'></div> -->
+        <div id="numbersTop" class="statsBox">
+          <div class="statsTitle"><span class="introTitle">B</span>y the numbers...</div>
+          <div>Population: <?php echo($totalPopulation) ?></div>
+          <div>Start Date: <?php echo($startDate) ?></div>
+          <div>End Date: <?php echo($endDate) ?></div>
+          <div>Location: Oxford, OH</div>
+          <div>BBS Counties: <?php echo($totalCounties) ?></div>
+          <div>BBS Cities: <?php echo($totalCities) ?></div>
+        </div>
+      </div>
+      <div id="hubContent">
+        <div class="govtLinkRow">
+          <div id="stateTop">
+            <div class="levelTitle">
+              STATE
+            </div>
+            <div class="sectionList">
+              <a href="state/governor/governor.php">
+                <div class="levelButton">
+                  <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
+                    <div>
+                      Office of the Governor
                     </div>
-                    <div class='statsRow'>
-                      <div><img src='img/flag_2.png'> ".$countyList[$countyNum]['flags']."</div>
-                      <div style='border-right:3px solid black; width: 2%;'></div>
-                      <div><img src='img/delegate_2.png'> ".$cntyPop."</div>
+                    <div>
+                      <div><img src='img/right_arrow.png'></div>
                     </div>
                   </div>
-                <!-- </a> -->"
-              );
-            };
-          ?>
-          </div>
-        </div>
-        <div id="cityTop">
-          <div class="levelTitle">
-            CITY
-          </div>
-          <div class="sectionList">
-          <?php
-            for ($cityNum = 0; $cityNum < count($cityList); $cityNum++) {
-              if ($cityList[$cityNum]['is_city'] == 0) {
-                echo(
-                  "<div class='subtitle'>
-                    <div><u>".$cityList[$cityNum]['section_name']." County</u></div>
+                </div>
+              </a>
+              <a href="state/congress/congress.php">
+                <div class="levelButton">
+                  <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
+                    <div>
+                      General Assembly
+                    </div>
+                    <div>
+                      <div><img src='img/right_arrow.png'></div>
+                    </div>
                   </div>
-                  "
-                );
-              } else {
+                </div>
+              </a>
+              <a href="state/supreme_court/supreme_court.php">
+                <div class="levelButton">
+                  <div class="sectionName" style="border-bottom:none;padding-bottom:0px">
+                    <div>
+                      Supreme Court
+                    </div>
+                    <div>
+                      <div><img src='img/right_arrow.png'></div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div id="countyTop">
+            <div class="levelTitle">
+              COUNTY
+            </div>
+            <div class="sectionList">
+            <?php
+              for ($countyNum = 0; $countyNum < count($countyList); $countyNum++) {
+                $cntyPopStmt = $pdo->prepare("SELECT SUM(population) FROM Section WHERE is_county=:si");
+                $cntyPopStmt->execute(array(
+                  ':si'=>htmlentities($countyList[$countyNum]['section_id'])
+                ));
+                $cntyPop = $cntyPopStmt->fetch(PDO::FETCH_ASSOC)['SUM(population)'];
                 echo("
-                  <!-- <a href='city/city.php?section_id=".$cityList[$cityNum]['section_id']."'> -->
+                  <!-- <a href='county/county.php?section_id=".$countyList[$countyNum]['section_id']."'> -->
                     <div class='levelButton'>
                       <div class='sectionName'>
-                        <div>".$cityList[$cityNum]['section_name']." City</div>
+                        <div>".$countyList[$countyNum]['section_name']." County</div>
                         <!-- <div><img src='img/right_arrow.png'></div> -->
                       </div>
                       <div class='statsRow'>
-                        <div><img src='img/flag_2.png'> ".$cityList[$cityNum]['flags']."</div>
+                        <div><img src='img/flag_2.png'> ".$countyList[$countyNum]['flags']."</div>
                         <div style='border-right:3px solid black; width: 2%;'></div>
-                        <div><img src='img/delegate_2.png'> ".$cityList[$cityNum]['population']."</div>
+                        <div><img src='img/delegate_2.png'> ".$cntyPop."</div>
                       </div>
                     </div>
                   <!-- </a> -->"
                 );
               };
-            };
-          ?>
+            ?>
+            </div>
+          </div>
+          <div id="cityTop">
+            <div class="levelTitle">
+              CITY
+            </div>
+            <div class="sectionList">
+            <?php
+              for ($cityNum = 0; $cityNum < count($cityList); $cityNum++) {
+                if ($cityList[$cityNum]['is_city'] == 0) {
+                  echo(
+                    "<div class='subtitle'>
+                      <div><u>".$cityList[$cityNum]['section_name']." County</u></div>
+                    </div>
+                    "
+                  );
+                } else {
+                  echo("
+                    <!-- <a href='city/city.php?section_id=".$cityList[$cityNum]['section_id']."'> -->
+                      <div class='levelButton'>
+                        <div class='sectionName'>
+                          <div>".$cityList[$cityNum]['section_name']." City</div>
+                          <!-- <div><img src='img/right_arrow.png'></div> -->
+                        </div>
+                        <div class='statsRow'>
+                          <div><img src='img/flag_2.png'> ".$cityList[$cityNum]['flags']."</div>
+                          <div style='border-right:3px solid black; width: 2%;'></div>
+                          <div><img src='img/delegate_2.png'> ".$cityList[$cityNum]['population']."</div>
+                        </div>
+                      </div>
+                    <!-- </a> -->"
+                  );
+                };
+              };
+            ?>
+            </div>
+          </div>
+        </div>
+        <div id="aboutTop">
+          <div class="levelTitle explainTitle">What Is Buckeye Boys State?</div>
+          <div class="explainBox">
+            <span class="introTitle">E</span>very summer, nearly 1000 young men take their first steps towards leading our nation by creating and running their own state: Buckeye Boys State (BBS). This 8 day-long camp, which takes place at Miami University in Oxford, OH, is a hands-on exercise in American democracy. During this time, the citizens of BBS will:
+            <ul>
+              <li>Run their own political parties</li>
+              <li>Run for elected position within their BBS cities, counties, or state</li>
+              <li>Vote on their own BBS election day</li>
+              <li>Take responsibility of their new duties within their BBS community (be they elected, appointed, or hired)</li>
+              <li>Fulfill their responsibility as a BBS citizen, such as abiding by the laws passed by the BBS government</li>
+            </ul>
+            For more information, check out the <a style="color:#fec231;border-bottom: 1px solid #fec231" href="http://www.ohiobuckeyeboysstate.com/">official American Legion website</a>.
           </div>
         </div>
       </div>
-      <div id="aboutTop">
-        <div class="levelTitle explainTitle">What Is Buckeye Boys State?</div>
-        <div class="explainBox">
-          <span class="introTitle">E</span>very summer, nearly 1000 young men take their first steps towards leading our nation by creating and running their own state: Buckeye Boys State (BBS). This 8 day-long camp, which takes place at Miami University in Oxford, OH, is a hands-on exercise in American democracy. During this time, the citizens of BBS will:
-          <ul>
-            <li>Run their own political parties</li>
-            <li>Run for elected position within their BBS cities, counties, or state</li>
-            <li>Vote on their own BBS election day</li>
-            <li>Take responsibility of their new duties within their BBS community (be they elected, appointed, or hired)</li>
-            <li>Fulfill their responsibility as a BBS citizen, such as abiding by the laws passed by the BBS government</li>
-          </ul>
-          For more information, check out the <a style="color:#fec231;border-bottom: 1px solid #fec231" href="http://www.ohiobuckeyeboysstate.com/">official American Legion website</a>.
-        </div>
+      <div class="applyLink footer" style="background-color:black">
+        Want to attend Buckeye Boys State next year?<br/>
+        <a href="http://www.ohiobuckeyeboysstate.com/">
+          <u>CLICK HERE!</u>
+        </a>
       </div>
-    </div>
-    <div class="applyLink footer" style="background-color:black">
-      Want to attend Buckeye Boys State next year?<br/>
-      <a href="http://www.ohiobuckeyeboysstate.com/">
-        <u>CLICK HERE!</u>
-      </a>
     </div>
   </body>
 </html>
