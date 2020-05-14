f7b9d85cea76f8189a468294ab5f27b2af133dc3
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-05-14 13:20:44 -0400

    uses  now

--- pdo.php
+++ pdo.php
@@ -10,7 +10,7 @@
     $jquery = "../../jquery.js";
   } else {
     $isLocal = false;
-    $pdo = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;port=3306;dbname=heroku_9f89bb0196fa398','bb859affb4aa30','*passwrd_goes_here*');
+    $pdo = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;port=3306;dbname=heroku_9f89bb0196fa398',$_ENV["CLEARDB_USERNAME"],$_ENV["CLEARDB_PASSWORD"]);
     $rootURL = $currentHost;
     $jquery = '"https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
