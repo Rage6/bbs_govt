aa9f495e4a3b5e30dc2f94a40960aab49ad5e5f6
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-07-22 13:18:51 -0400

    blocks based on error cookie

--- admin/login/lead_login.php
+++ admin/login/lead_login.php
@@ -11,6 +11,10 @@ if (isset($_POST['sectionLogin'])) {
   $givenSect = htmlentities($_POST['sectionId']);
   $givenPw = htmlentities($_POST['sectionPw']);
 
+  if (!isset($_COOKIE['error'])) {
+    setcookie('error','start');
+  };
+
   // Route to locksmith page...
   if ($givenSect == "999") {
     $masterStmt = $pdo->prepare("SELECT key_pw,attempts FROM Maintenance WHERE locksmith_id=999");
@@ -43,7 +47,7 @@ if (isset($_POST['sectionLogin'])) {
     };
 
   // Route to admin page...
-} elseif ($givenSect != "") {
+  } elseif ($givenSect != "") {
     $findSecStmt = $pdo->prepare('SELECT * FROM Section WHERE section_id=:sid');
     $findSecStmt->execute(array(
       ':sid'=>htmlentities($_POST['sectionId'])
@@ -54,15 +58,29 @@ if (isset($_POST['sectionLogin'])) {
     if (count($secInfo) != 1) {
       $_SESSION['message'] = "<b style='color:red'>ERROR 2 (login.php): More/less than 1 row in 'secInfo'</b>";
       header('Location: login.php');
+      return false;
     } else {
-      if ($secInfo[0]['couns_num'] > 4 || $secInfo[0]['del_num'] > 4) {
-        $_SESSION['message'] = "<b style='color:red'>Due to multiple failed password attempts, this section is currently locked. You must contact the BBS IT staff in order to unlock this section.</b>";
-        header('Location: login.php');
-        return false;
+      // Blocks too many attempts from by the same user
+      $errorInfo = null;
+      if ($_COOKIE['error'] != 'start') {
+        $blockListStmt = $pdo->prepare("SELECT * FROM Blocked");
+        $blockListStmt->execute();
+        while ($oneBlocked = $blockListStmt->fetch(PDO::FETCH_ASSOC)) {
+          if ($oneBlocked['error_cookie'] == $_COOKIE['error']) {
+            $errorInfo = $oneBlocked;
+            if ($errorInfo['blocked'] == 1) {
+              $_SESSION['message'] = "<b style='color:red'>You failed to log in at least 5 times.</br> Talk to the BBS IT Staff to unlock your password.</b>";
+              header('Location: login.php');
+              return false;
+            };
+          };
+        };
       };
+      //
       $newTkn = bin2hex(random_bytes(64));
       if (password_verify($givenPw,$secInfo[0]['couns_pw'])) {
-        $counsTknStmt = $pdo->prepare("UPDATE Section SET couns_token=:nt, couns_num=0, del_num=0 WHERE section_id=:scd");
+        // $counsTknStmt = $pdo->prepare("UPDATE Section SET couns_token=:nt, couns_num=0, del_num=0 WHERE section_id=:scd");
+        $counsTknStmt = $pdo->prepare("UPDATE Section SET couns_token=:nt WHERE section_id=:scd");
         $counsTknStmt->execute(array(
           ':nt'=>$newTkn,
           ':scd'=>htmlentities($_POST['sectionId'])
@@ -70,15 +88,27 @@ if (isset($_POST['sectionLogin'])) {
         $_SESSION['counsToken'] = $newTkn;
         $_SESSION['secId'] = htmlentities($_POST['sectionId']);
         $_SESSION['message'] = "<b style='color:green'>Counselor login successful</b>";
-        $counsSessionStmt = $pdo->prepare('UPDATE Section SET couns_sess_start=:st, failed_IP=NULL WHERE section_id=:sd');
+        // $counsSessionStmt = $pdo->prepare('UPDATE Section SET couns_sess_start=:st, failed_IP=NULL WHERE section_id=:sd');
+        $counsSessionStmt = $pdo->prepare('UPDATE Section SET couns_sess_start=:st WHERE section_id=:sd');
         $counsSessionStmt->execute(array(
           ':st'=>time(),
           ':sd'=>htmlentities($_SESSION['secId'])
         ));
+        // Removes the 'error' cookie (if present);
+        if (isset($_COOKIE['error'])) {
+          $deleteCounsErrorStmt = $pdo->prepare("DELETE FROM Blocked WHERE error_cookie = :ce");
+          $deleteCounsErrorStmt->execute(array(
+            ':ce'=>htmlentities($_COOKIE['error'])
+          ));
+        };
+        unset($_COOKIE['error']);
+        $removeCounsError = setcookie('error','',time() - 3600);
+        //
         header('Location: ../admin/admin.php');
         return true;
       } elseif (password_verify($givenPw,$secInfo[0]['del_pw'])) {
-        $delTknStmt = $pdo->prepare("UPDATE Section SET del_token=:nt, couns_num=0, del_num=0 WHERE section_id=:scd");
+        // $delTknStmt = $pdo->prepare("UPDATE Section SET del_token=:nt, couns_num=0, del_num=0 WHERE section_id=:scd");
+        $delTknStmt = $pdo->prepare("UPDATE Section SET del_token=:nt WHERE section_id=:scd");
         $delTknStmt->execute(array(
           ':nt'=>$newTkn,
           ':scd'=>htmlentities($_POST['sectionId'])
@@ -86,41 +116,55 @@ if (isset($_POST['sectionLogin'])) {
         $_SESSION['delToken'] = $newTkn;
         $_SESSION['secId'] = htmlentities($_POST['sectionId']);
         $_SESSION['message'] = "<b style='color:green'>Delegate login successful</b>";
-        $delSessionStmt = $pdo->prepare('UPDATE Section SET del_sess_start=:st, failed_IP=NULL WHERE section_id=:sd');
+        // $delSessionStmt = $pdo->prepare('UPDATE Section SET del_sess_start=:st, failed_IP=NULL WHERE section_id=:sd');
+        $delSessionStmt = $pdo->prepare('UPDATE Section SET del_sess_start=:st WHERE section_id=:sd');
         $delSessionStmt->execute(array(
           ':st'=>time(),
           ':sd'=>htmlentities($_SESSION['secId'])
         ));
+        // Removes the 'error' cookie (if present);
+        if (isset($_COOKIE['error'])) {
+          $deleteDelErrorStmt = $pdo->prepare("DELETE FROM Blocked WHERE error_cookie = :de");
+          $deleteDelErrorStmt->execute(array(
+            ':de'=>htmlentities($_COOKIE['error'])
+          ));
+        };
+        unset($_COOKIE['error']);
+        $removeDelError = setcookie('error','',time() - 3600);
+        //
         header('Location: ../admin/admin.php');
         return true;
       } else {
-        if ($secInfo[0]['del_num'] == 4) {
-          if ($secInfo[0]['failed_IP'] == null) {
-            $failedIPStmt = $pdo->prepare("UPDATE Section SET failed_IP=:fi WHERE section_id=:si");
-            $failedIPStmt->execute(array(
-              ':fi'=>htmlentities($_SERVER['REMOTE_ADDR']),
-              ':si'=>htmlentities($_POST['sectionId'])
+        if ($errorInfo == null) {
+          $errorTag = bin2hex(random_bytes(64));
+          $addToBlockStmt = $pdo->prepare("INSERT INTO Blocked(error_cookie,time_stamp) VALUES (:et,:ti)");
+          $addToBlockStmt->execute(array(
+            ':et'=>htmlentities($errorTag),
+            ':ti'=>time()
+          ));
+          setcookie('error', $errorTag);
+          $totalMessage = "You have 4 login attempts remaining.";
+        } else {
+          if ($errorInfo['error_count'] + 1 >= 5) {
+            $newBlockStmt = $pdo->prepare("UPDATE Blocked SET error_count = error_count + 1, blocked = 1 WHERE error_cookie = :ei");
+            $newBlockStmt->execute(array(
+              ':ei'=>htmlentities($errorInfo['error_cookie'])
             ));
+            $totalMessage = "You have run out of login attempts.</br> Contact the BBS IT Staff for assistance.";
+          } else {
+            $updateBlockStmt = $pdo->prepare("UPDATE Blocked SET error_count = error_count + 1 WHERE error_cookie = :eu");
+            $updateBlockStmt->execute(array(
+              ':eu'=>htmlentities($errorInfo['error_cookie'])
+            ));
+            $countsLeft = 5 - ($errorInfo['error_count'] + 1);
+            $totalMessage = "You have ".$countsLeft." attempts remaining.";
           };
         };
-        $numDelFails = $secInfo[0]['del_num'] + 1;
-        $numLeft = 5 - $numDelFails;
-        if ($numLeft <= 0) {
-          $totalMessage = "Your section is now locked. Contact the BBS IT staff in order to unlock this section.";
-        } else {
-          $totalMessage = "You have ".$numLeft." more attempts until this section locks.";
-        };
-        $addNumStmt = $pdo->prepare("UPDATE Section SET del_num = del_num + 1, couns_num = couns_num + 1 WHERE section_id=:scd");
-        $addNumStmt->execute(array(
-          ':scd'=>htmlentities($_POST['sectionId'])
-        ));
         $_SESSION['message'] = "<b style='color:red'>Password Incorrect.</br> ".$totalMessage."</b>";
         header('Location: login.php');
-        return fail;
+        return false;
       };
     };
-    header('Location: ../admin/admin.php');
-    return true;
 
   // Route back to login when no section is selected
   } else {
@@ -130,6 +174,9 @@ if (isset($_POST['sectionLogin'])) {
   };
 };
 
+// echo("</br>");
+// echo("</br>");
+// echo("</br>");
 // echo("GET:");
 // echo("<pre>");
 // var_dump($_GET);
@@ -142,5 +189,8 @@ if (isset($_POST['sectionLogin'])) {
 // echo("<pre>");
 // var_dump($_SESSION);
 // echo("</pre>");
-
+// echo("COOKIE:");
+// echo("<pre>");
+// var_dump($_COOKIE);
+// echo("</pre>");
 ?>
