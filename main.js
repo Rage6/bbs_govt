96296bafc2a9a66fa4e645bfd54bfd73cb575978
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-09-04 01:17:25 -0400

    added BAR exam for judges,directors

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ $(() => {
       level: "state",
       fee: 700,
       paired: "The Governor must run with a Lt. Governor.",
-      eventList: [1,2],
+      eventList: [3,4,5,6,18,19,21,22,23,24,25]
     },
     {
       positionID: 2,
@@ -144,182 +144,182 @@ $(() => {
       level: "state",
       fee: 700,
       paired: "The Lt. Governor must run with a Governor. His campaign fee is combined with that of the Governor, for a total of $700.",
-      eventList: [1,2]
+      eventList: [3,4,5,6,18,19,21,22,23,24,25]
     },
     {
       positionID: 3,
       name: "Attorney General",
       level: "state",
       fee: 300,
-      eventList: [1,2]
+      eventList: [3,4,5,8,18,19,21,22,23,24,25]
     },
     {
       positionID: 4,
       name: "Treasurer",
       level: "state",
       fee: 300,
-      eventList: [1,2]
+      eventList: [3,4,5,8,18,19,21,22,23,24,25]
     },
     {
       positionID: 5,
       name: "Auditor",
       level: "state",
       fee: 300,
-      eventList: [1,2]
+      eventList: [3,4,5,8,18,19,21,22,23,24,25]
     },
     {
       positionID: 6,
       name: "Secretary of State",
       level: "state",
       fee: 300,
-      eventList: [1,2]
+      eventList: [3,4,5,8,18,19,21,22,23,24,25]
     },
     {
       positionID: 7,
       name: "Chief Justice",
       level: "state",
       fee: 400,
-      eventList: [1,2]
+      eventList: [3,26,4,5,7,18,19,21,27,22,28,23,24,25]
     },
     {
       positionID: 8,
       name: "Associate Justice",
       level: "state",
       fee: 300,
-      eventList: [1,2]
+      eventList: [3,26,4,5,8,18,19,21,27,22,28,23,24,25]
     },
     {
       positionID: 9,
       name: "Judge of the Court of Appeals",
       level: "state",
       fee: 200,
-      eventList: [1,2]
+      eventList: [3,26,4,5,9,18,19,21,27,22,28,23,24,25]
     },
     {
       positionID: 10,
       name: "State Senator",
       level: "state",
       fee: 75,
-      eventList: [1,2]
+      eventList: [5,11,15,16,18,20,24,25]
     },
     {
       positionID: 11,
       name: "State Representative",
       level: "state",
       fee: 75,
-      eventList: [1,2]
+      eventList: [5,11,15,16,18,20,24,25]
     },
     {
       positionID: 12,
       name: "Commissioner",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 13,
       name: "Treasurer",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 14,
       name: "Engineer",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 15,
       name: "Recorder",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 16,
       name: "Auditor",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 17,
       name: "Prosecuting Attorney",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 18,
       name: "Clerk of Courts",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,24,25]
     },
     {
       positionID: 19,
       name: "Judge of Municipal Court",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,26,27,28,24,25]
     },
     {
       positionID: 20,
       name: "Judge of the Court of Common Pleas",
       level: "county",
       fee: 150,
-      eventList: [1,2]
+      eventList: [14,17,5,10,18,20,26,27,28,24,25]
     },
     {
       positionID: 21,
       name: "State School Board Member",
       level: "county",
       fee: 1,
-      eventList: [1,2]
+      eventList: [14,17,5,13,18,20,24,25]
     },
     {
       positionID: 22,
       name: "Mayor",
       level: "city",
       fee: 50,
-      eventList: [1,2]
+      eventList: [15,16,5,12,18,20,24,25]
     },
     {
       positionID: 23,
       name: "Treasurer",
       level: "city",
       fee: 50,
-      eventList: [1,2]
+      eventList: [15,16,5,12,18,20,24,25]
     },
     {
       positionID: 24,
       name: "Auditor",
       level: "city",
       fee: 50,
-      eventList: [1,2]
+      eventList: [15,16,5,12,18,20,24,25]
     },
     {
       positionID: 25,
       name: "Director of Law",
       level: "city",
       fee: 50,
-      eventList: [1,2]
+      eventList: [26,15,16,5,12,18,20,27,24,28,25]
     },
     {
       positionID: 26,
       name: "Councilman",
       level: "city",
       fee: 50,
-      eventList: [1,2]
+      eventList: [15,16,5,12,18,20,24,25]
     },
     {
       positionID: 27,
       name: "City School Board Member",
       level: "city",
       fee: 1,
-      eventList: [1,2]
+      eventList: [15,16,5,13,18,20,24,25]
     },
   ];
 
@@ -346,91 +346,91 @@ $(() => {
     },
     {
       eventID: 6,
-      eventName: "Collect $700 for the Governor & Lt. Governor fee (in total)",
+      eventName: "Pay $700 for the Governor & Lt. Governor fee (in total)",
       description: "Testing",
       time: ""
     },
     {
       eventID: 7,
-      eventName: "Collect $400 fee",
+      eventName: "Pay $400 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 8,
-      eventName: "Collect $300 fee",
+      eventName: "Pay $300 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 9,
-      eventName: "Collect $200 fee",
+      eventName: "Pay $200 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 10,
-      eventName: "Collect $150 fee",
+      eventName: "Pay $150 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 11,
-      eventName: "Collect $75 fee",
+      eventName: "Pay $75 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 12,
-      eventName: "Collect $50 fee",
+      eventName: "Pay $50 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 13,
-      eventName: "Collect $1 fee",
+      eventName: "Pay $1 fee",
       description: "Testing",
       time: ""
     },
     {
       eventID: 14,
-      eventName: "Fill out the Declaration of Candidacy (DoC) form",
-      description: "Testing",
-      time: ""
-    },
-    {
-      eventID: 15,
-      eventName: "Submit the completed DoC, fee, and petition",
-      description: "Testing",
-      time: ""
-    },
-    {
-      eventID: 16,
-      eventName: "Submit the completed DoC and fee",
-      description: "Testing",
-      time: ""
-    },
-    {
-      eventID: 17,
       eventName: "Recieve a nomination during county party caucus",
       description: "Testing",
       time: ""
     },
     {
-      eventID: 18,
+      eventID: 15,
       eventName: "Recieve a nomination during city party caucus",
       description: "Testing",
       time: ""
     },
     {
-      eventID: 19,
+      eventID: 16,
       eventName: "Win the majority of the votes during the city caucus",
       description: "Testing",
       time: ""
     },
     {
-      eventID: 20,
+      eventID: 17,
       eventName: "Win the majority of the votes during the county caucus",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 18,
+      eventName: "Fill out the Declaration of Candidacy (DoC) form",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 19,
+      eventName: "Submit the completed DoC, fee, and petition",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 20,
+      eventName: "Submit the completed DoC and fee",
       description: "Testing",
       time: ""
     },
@@ -461,6 +461,24 @@ $(() => {
     {
       eventID: 25,
       eventName: "Submit the complete 'Campaign Contribution & Expenditure' form",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 26,
+      eventName: "Take BAR exam (1st attempt)",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 27,
+      eventName: "Take BAR exam (2nd attempt)",
+      description: "Testing",
+      time: ""
+    },
+    {
+      eventID: 28,
+      eventName: "Take BAR exam (final attempt)",
       description: "Testing",
       time: ""
     }
